2d99634b1442e0bd763d7b49e7c01798fe3c8205
Jordan <jordanrob709@gmail.com>
2022-06-30 23:29:47 +0300

    added more test routes for security actions

--- routes/secActionRoutes.js
+++ routes/secActionRoutes.js
@@ -7,7 +7,31 @@ router.get("/status", asyncHandler( async (req, res) => {
     
     let msg 
 
-    cp.exec('ls', (err, stdout, stderr) => {
+    cp.exec('pwd', (err, stdout, stderr) => {
+        console.log('#1, exec')
+        console.log(stdout)
+        msg = stdout
+        res.json({message:`${msg}`})
+    })
+}))
+
+router.get("/net", asyncHandler( async (req, res) => {
+    
+    let msg 
+
+    cp.exec('ifconfig', (err, stdout, stderr) => {
+        console.log('#1, exec')
+        console.log(stdout)
+        msg = stdout
+        res.json({message:`${msg}`})
+    })
+}))
+
+router.get("/system-user", asyncHandler( async (req, res) => {
+    
+    let msg 
+
+    cp.exec('whoami', (err, stdout, stderr) => {
         console.log('#1, exec')
         console.log(stdout)
         msg = stdout
