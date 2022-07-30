ac3875e9c9f6523a6af2689134e642589a2279c8
Jordan <jordanrob709@gmail.com>
2022-07-30 11:31:03 +0300

    cleaned up client calls to twillio | added conditionals for all rules set up in snort local rules

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const { registerUser, loginUser, getMe } = require('../controllers/userController.js')
-const protect = require('')
+const protect = require('../middleware/authMiddleware')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
-router.get('/me', getMe)
+router.get('/me', protect, getMe)
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const secRoutes = require('./routes/secActionRoutes')
+const userRoutes = require('./routes/userRoutes')
 const connectDB = require('./config/db')
 
 const port = process.env.PORT || 5000
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: false }))
 
 
 app.use('/api/sec-actions', secRoutes)
+app.use('/api/users', userRoutes)
 
 
 app.listen(port, () => console.log(`server started on ${port}`))
